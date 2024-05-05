197be8d7c166045c013f357a4b6f4a2830ae198f
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-05 19:23:35 +0300

    stop game when all states matched

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 from data import Data
+from turtle_states import State
 
 screen = turtle.Screen()
 image = "blank_states_img.gif"
@@ -7,8 +8,13 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = Data()
+state = State()
 
-answer = screen.textinput(title="Guess The States", prompt="What is the state's name?")
-data.check_answer(answer)
+while data.remaining_states > 0:
+    answer = screen.textinput(title="Guess The States", prompt="What is the state's name?")
+
+    if data.check_answer(answer):
+        x_cor, y_cor = data.get_state_cor(answer)
+        state.write_state(x_cor=x_cor, y_cor=y_cor, state_name=answer)
 
 screen.exitonclick()
